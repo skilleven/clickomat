53196f9b0b6c412371c1339f4b6eae1b5b4e7474
Torsten Hoeltge <th@skilleven.de>
2022-09-26 14:34:30 +0200

    setup -> removed find_packages

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 DESCRIPTION = 'Python-Script for automated click-sequences and text entry'
 LONG_DESCRIPTION = 'The purpose of Clickomat is to map the sometimes relatively long python commands from pyautogui with short, single-line commands that are processed in a simple text list.'
 
@@ -16,7 +16,6 @@ setup(
     long_description=LONG_DESCRIPTION,
     py_modules=['clickomat'],
     package_dir={'':'src'},
-    # packages=find_packages(),
     install_requires=['pyautogui', 'keyboard', 'easygui', 'opencv-python', 'pillow'],
     keywords=['python', 'mouse-automation', 'gui-testing', 'automation'],
     classifiers=[
