aa37d737b645c2c1b95eb36d873da1f434ca9e8e
Torsten Hoeltge <th@skilleven.de>
2022-09-29 14:18:04 +0200

    v0.2.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name="clickomat",
-    version='0.2.1',
+    version='0.2.2',
     entry_points={
         'console_scripts': [
             'clickomat=clickomat:run'
