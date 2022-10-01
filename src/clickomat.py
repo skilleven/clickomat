0b56e8d00ac4d04de91b28b22f5c9e1dc285049c
Torsten Hoeltge <th@skilleven.de>
2022-10-01 15:48:28 +0200

    v0.3.2

--- src/clickomat.py
+++ src/clickomat.py
@@ -8,7 +8,7 @@ if os.name == 'nt': import msvcrt
 
 #-----------------------------------------------------
 #
-# Clickomat v0.3.1
+# Clickomat v0.3.2
 #
 #-----------------------------------------------------
 
@@ -646,7 +646,7 @@ def run(version,path,clicklist,images,confidence,autoswitch,silent,step,noswitch
     """Clickomat documentation is available under https://github.com/skilleven/clickomat/wiki"""
 
     if version:
-        print("Clickomat 0.3.1 is installed.\nYou may want to check if your version is up to date: pip list --outdated")
+        print("Clickomat 0.3.2 is installed.\nYou may want to check if your version is up to date: pip list --outdated")
         exit()
 
     case_path = path
